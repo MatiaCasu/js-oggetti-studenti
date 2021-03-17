3daa07b67f4c52449f259ed2b39a5befa596af52
Matia <matia.casu@gmail.com>
2021-03-17 16:12:03 +0100

    Creata array studenti e stampati nome, cognome

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
-/* Creare un array di oggetti di studenti.
-Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+/* Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
 
-/* Oggetto studente con proprietà: nome, cognome e età. */
-var studente = {
-  nome: "matia",
-  cognome: "casu",
-  eta: 26
-};
-/* /Oggetto studente con proprietà: nome, cognome e età. */
+// /* Oggetto studente con proprietà: nome, cognome e età. */
+// var studente = {
+//   nome: "matia",
+//   cognome: "casu",
+//   eta: 26
+// };
+// /* /Oggetto studente con proprietà: nome, cognome e età. */
+//
+// /* Stampa a schermo le proprietà dell'oggetto con un ciclo for-in */
+// var proprieta = "";
+// for(var key in studente){
+//   proprieta += "<br>" + studente[key];
+// }
+//
+// $("p").append(proprieta);
+// /* /Stampa a schermo le proprietà dell'oggetto con un ciclo for-in*/
 
-/* Stampa a schermo le proprietà dell'oggetto con un ciclo for-in */
-var proprieta = "";
-for(var key in studente){
-  proprieta += "<br>" + studente[key];
+/* Creare un array di oggetti di studenti */
+var studenti = [
+  {
+    nome: "matia",
+    cognome: "casu",
+    eta: 26
+  },
+  {
+    nome: "franco",
+    cognome: "pippo",
+    eta: 32
+  },
+  {
+    nome: "pluto",
+    cognome: "brambilla",
+    eta: 20
+  },
+];
+/* /Creare un array di oggetti di studenti */
+
+/* Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
+var proprieta = "<br>";
+
+for(var i = 0; i < studenti.length; i++){
+
+  proprieta += studenti[i]["nome"]+ " " + studenti[i]["cognome"] + "<br>";
 }
 
 $("p").append(proprieta);
-/* /Stampa a schermo le proprietà dell'oggetto con un ciclo for-in*/
+/* /Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
